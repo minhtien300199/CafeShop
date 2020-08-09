fde1f88dc76c1f75fd0d8f6ffefd7fa427f1a219
Vo Minh Hieu <43235486+minhhieu023@users.noreply.github.com>
2020-08-09 15:12:45 +0700

    change login

--- lib/Services/UserAPI.dart
+++ lib/Services/UserAPI.dart
@@ -8,14 +8,22 @@ import '../Models/Product.dart';
 class UserAPI {
   User _user;
   Future<String> loginUser(String userName, String password) async {
-    String url = "https://caffeeshopbackend.herokuapp.com/login";
-    String json = '{"userName": "$userName", "password": "$password"}';
+    String url = "http://192.168.1.2:8000/login";
+    print(userName);
+    print(password);
+    String json = '{"userName": "$userName", "password": $password}';
+    print(json);
 
-    Response response = await post(url, body: json);
+    Response response = await post(url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: json);
 
     if (response.statusCode == 200) {
       String userInfo = response.body;
       print(userInfo);
+      // if( userInfo.status == "OK roi")
       return userInfo;
     } else {
       throw Exception('Fail to fetch data');
